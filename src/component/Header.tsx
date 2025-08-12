b9f1f6dd55128bf00dc7664eac4fcda644a5e293
sispo3314 <sispo3314@gmail.com>
2025-08-12 15:11:11 +0900

    feat: 헤더 길이 max width에 맞추게 리팩토링

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,6 +1,6 @@
 import { Menu, User } from 'react-feather';
 import { useNavigate } from 'react-router-dom';
-import Logo from '../image/Logo.png'; 
+import Logo from '../image/Logo.png';
 
 type HeaderProps = {
   onMenuClick: () => void;
@@ -10,27 +10,24 @@ export default function Header({ onMenuClick }: HeaderProps) {
   const navigate = useNavigate();
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 w-full bg-[#f8fbe9] border-b border-gray-200">
-      <div className="w-full flex flex-row items-center justify-between px-4 py-2">
+    <header className="bg-beige3 fixed top-0 right-0 left-0 z-50 mx-auto flex h-[44px] w-full max-w-[430px] items-center justify-between">
+      <div className="flex w-full flex-row items-center justify-between px-4 py-2">
         {/* 왼쪽: 메뉴(햄버거) 아이콘 */}
-        <Menu
-          className="w-7 h-7 text-green-700 cursor-pointer"
-          onClick={onMenuClick}
-        />
+        <Menu className="h-7 w-7 cursor-pointer text-green-700" onClick={onMenuClick} />
 
         {/* 가운데: 로고 이미지 */}
-        <div className="flex-1 flex justify-center">
+        <div className="flex flex-1 justify-center">
           <img
             src={Logo}
             alt="logo"
-            className="h-8 object-contain cursor-pointer"
+            className="h-8 cursor-pointer object-contain"
             onClick={() => navigate('/')}
           />
         </div>
 
         {/* 오른쪽: 사람 아이콘 */}
         <User
-          className="w-7 h-7 text-green-700 cursor-pointer"
+          className="h-7 w-7 cursor-pointer text-green-700"
           onClick={() => navigate('/mypage')}
         />
       </div>
