0c6058d5feae4b903d6cc169d213ca2ae66393e4
sispo3314 <sispo3314@gmail.com>
2025-09-20 20:51:03 +0900

    fix: 카드 컴포넌트 UI 수정

--- src/constants/MyTravel.tsx
+++
@@ -1,6 +0,0 @@
-export const TRAVEL_ITEMS = [
-  { id: 1, title: '행궁동', tranquil: true, type: '자연관광지', likes: 3 },
-  { id: 2, title: '행궁동', tranquil: true, type: '자연관광지', likes: 3 },
-  { id: 3, title: '행궁동', tranquil: true, type: '자연관광지', likes: 3 },
-  { id: 4, title: '행궁동', tranquil: true, type: '자연관광지', likes: 3 },
-];
